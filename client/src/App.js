108c5481fbcf6814acd24b1afddd8363bf8c9ebc
SIERRADIGITALIN\k.paramasivam <k.paramasivam@sierradigitalinc.com>
2023-07-21 18:24:29 +0530

    fix home page not redirect issue

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
-              <Route path="/" element={<Navigate path="/dashboard" replace/>} />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard"  element={ <Dashboard /> }/>
               <Route path="/products"  element={ <Products /> }/>
               <Route path="/customers"  element={ <Customers /> }/>
